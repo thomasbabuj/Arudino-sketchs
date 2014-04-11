0e3b3496bb05a220cb4795a815ac9c594ce104e4
Thomas Babu <tommy.developer@gmail.com>
2014-04-11 13:13:29 +0800

    Initial Commits

---
+++ Circuit__02/Circuit__02.ino
@@ -0,0 +1,43 @@
+/*
+  POTENTIOMETER
+  
+  Measure the poistion of a potentiometer and use it to 
+  control the blink rate of an LED. Turn the knob to make 
+  it blink faster or slower!
+  
+  Its a same type of control we'd use to change volume,
+  dim a lamp, etc. A potentiometer changes resistance as 
+  it is turned.
+  
+  LED blink delay based on the potentiometer value
+*/
+
+int sensorPin = 0;  // The potentiometer is connect to analog
+                    // pin 0
+int ledPin = 13;  // LED is connected to digital pin 13
+
+void setup() {
+   pinMode(ledPin, OUTPUT); 
+   Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue;
+  
+  sensorValue = analogRead(sensorValue);
+  
+  Serial.print("Sesnor = ");
+  Serial.print(sensorValue);
+  Serial.print("\n");
+ 
+  
+  digitalWrite(ledPin, HIGH); // Turn the LED ON
+  
+  delay( sensorValue ); // Pause for sensorValue milliseconds
+  
+  digitalWrite(ledPin, LOW); // Turn the LED OFF
+  
+  delay(sensorValue);
+}
+
+
